6aaa019e3da8342cb59e40794d527ab3fb14fff6
venkeychintha <venkeychintha97@gmail.com>
2019-09-28 12:54:43 +0530

    water

--- hungry.py
+++ hungry.py
@@ -3,4 +3,6 @@ if hungry == "yes":
     print('eat samosa')
     print('eat pizza')
 else:
-    print('do your homework')
+    thirsty = input('are you thirsty:')
+    if thirsty == "yes":
+        print('Drink water')
